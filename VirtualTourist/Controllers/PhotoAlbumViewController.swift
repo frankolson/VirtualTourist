5bac4c95d33a20ec1937e5512fe5ec706640db80
Will Olson <will.olson11@gmail.com>
2021-08-14 21:49:43 -0500

    Setup project storyboard

---
+++ VirtualTourist/Controllers/PhotoAlbumViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoAlbumViewController.swift
+//  VirtualTourist
+//
+//  Created by Will Olson on 8/14/21.
+//
+
+import UIKit
+
+class PhotoAlbumViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
--- VirtualTourist/Controllers/TravelLocationsMapViewController.swift
+++ VirtualTourist/Controllers/TravelLocationsMapViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TravelLocationsMapViewController.swift
 //  VirtualTourist
 //
 //  Created by Will Olson on 8/14/21.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TravelLocationsMapViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
